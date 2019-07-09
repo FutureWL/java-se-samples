ee338aba2008cc01875e2d96cbb9cde1279a8936
FutureWL <624263934@qq.com>
2019-07-10 00:31:33 +0800

    更新

---
+++ spring-boot/spring-boot-servlet/src/main/java/io/github/futurewl/samples/SpringBootServletApplication.java
@@ -0,0 +1,11 @@
+package io.github.futurewl.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServletApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServletApplication.class, args);
+    }
+}
