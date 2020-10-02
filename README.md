54e2d9258037b3aedf684ecd63e8c5aca2bbddac
FutureWL <624263934@qq.com>
2020-10-02 19:22:50 +0800

    更新：初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Java Se Samples
 
+- 基础语法
+- 对象和类
+- 基本数据类型
+- 变量类型
+- 修饰符
+- 运算符
+- 循环结构
+- 条件语句
+- switch case
+- Number & Math
+- Character
+- String
+- StringBuffer
--- src/main/java/cn/huntercat/demo1/Main01.java
+++ src/main/java/cn/huntercat/demo1/Main01.java
@@ -1,5 +1,8 @@
-package cn.huntercat.demo;
+package cn.huntercat.demo1;
 
+/**
+ * 案例 1 基础语法
+ */
 public class Main01 {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
